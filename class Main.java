282fa2939d08890b087f8f35c8beffa4e693b423
TommyCoReviews <darwinevangelista30@gmail.com>
2026-06-23 16:48:11 +0800

    Trial

---
+++ class Main.java
@@ -0,0 +1,17 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+        System.out.println("World is Bad");
+    }
+}
+
+//data types
+//int, double, char, boolean, String
+
+//identifiers
+//must start with a letter, underscore, or dollar sign cant use numbers
+
+//variables
+//must be declared before use
